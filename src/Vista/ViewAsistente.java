fb04f5678afff06e38daeede5deddac23e03108c
ChristianC81 <christian.crespo.est@tecazuay.edu.ec>
2022-08-24 20:32:41 -0500

    Cambio Vista Asistente Prueba

--- src/Vista/ViewAsistente.java
+++ src/Vista/ViewAsistente.java
@@ -46,6 +46,11 @@ public class ViewAsistente extends javax.swing.JFrame {
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 180, -1));
 
         jButton2.setText("jButton2");
@@ -114,6 +119,11 @@ public class ViewAsistente extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
