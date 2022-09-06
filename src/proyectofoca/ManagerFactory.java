1d060251031a3ac548aa23e7a569884f7f9b9057
Miriam Medina <miriam.medina.est@tecazuay.edu.ec>
2022-09-06 07:31:05 -0500

    Agregar los modelos tabla, y manager entity factory

---
+++ src/proyectofoca/ManagerFactory.java
@@ -0,0 +1,28 @@
+
+package proyectofoca;
+
+import java.sql.Connection;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author miri
+ */
+public class ManagerFactory {
+    private EntityManagerFactory emf=null;
+    public EntityManagerFactory getEmf(){
+        return emf = Persistence.createEntityManagerFactory("ProyectoFOCAPU");
+    }
+    
+    // manager
+
+public static final Connection getConnection(final EntityManager entityManager) {
+        entityManager.getTransaction().begin();
+        Connection connection = entityManager.unwrap(java.sql.Connection.class);
+        return connection;
+
+    }
+    
+}
